1af54ab8ecb128780228b0dfdeaba69f5ac0fd9a
Shahrukh Raza <shahrukh4062@gmail.com>
2023-03-25 20:19:04 +0900

    done

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:02:55 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/03/24 12:18:26 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/03/25 17:09:32 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,59 @@
 #include <stdlib.h>
 #include"./libs/ft_printf/ft_printf.h"
 #include"./libs/libft/libft.h"
+
+// void	push_signal(int bit, int pid)
+// {
+// 	if (bit == 0)
+// 		kill(pid, SIGUSR1);
+// 	else if (bit == 1)
+// 		kill(pid, SIGUSR2);
+// 	usleep(500);
+// }
+
+// void	ft_convert_signal(unsigned int argv, int pid)
+// {
+// 	char	bit[9];
+// 	int		i;
+
+// 	bit[8] = '\0';
+// 	i = 7;
+// 	while (argv >= 1 || i >= 0)
+// 	{
+// 		if (argv >= 1)
+// 		{
+// 			bit[i] = argv % 2;
+// 			argv = argv / 2;
+// 		}
+// 		else
+// 			bit[i] = 0;
+// 		i--;
+// 	}
+// 	i = 0;
+// 	while (i < 8)
+// 	{
+// 		push_signal(bit[i], pid);
+// 		i++;
+// 	}
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	int	i;
+// 	int	pid;
+
+// 	i = 0;
+// 	if (argc != 3)
+// 		return (0);
+// 	pid = ft_atoi(argv[1]);
+// 	while (argv[2][i])
+// 	{
+// 		ft_convert_signal((int)argv[2][i], pid);
+// 		i++;
+// 	}
+// }
+
+
 
 void	push_signal(int bit, int pid)
 {
@@ -51,18 +104,28 @@ void	ft_convert_signal(unsigned int argv, int pid)
 	}
 }
 
+void	message_recieved(int sign)
+{
+	(void)sign;
+	write(1, "Message Recieved!!\n", 20);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	pid;
+	int					i;
+	int					pid;
 
 	i = 0;
 	if (argc != 3)
 		return (0);
 	pid = ft_atoi(argv[1]);
+	signal(SIGUSR1, message_recieved);
 	while (argv[2][i])
 	{
 		ft_convert_signal((int)argv[2][i], pid);
 		i++;
 	}
+	while (1)
+		pause();
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:03:02 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/03/23 12:51:30 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/03/25 17:11:12 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,59 @@
 #include "./libs/ft_printf/ft_printf.h"
 #include "./libs/libft/libft.h"
 
-char	ft_convert_demical(int num)
+// char	ft_convert_demical(int num)
+// {
+// 	int	demical;
+// 	int	base;
+
+// 	demical = 0;
+// 	base = 1;
+// 	while (num > 0)
+// 	{
+// 		demical = demical + (num % 10) * base;
+// 		num = num / 10;
+// 		base = base * 2;
+// 	}
+// 	return ((char)demical);
+// }
+
+// void	ft_handler(int signalType)
+// {
+// 	static int	num;
+// 	static int	counter;
+// 	char		c;
+
+// 	if (signalType == SIGUSR1)
+// 		num = (num * 10) + 0;
+// 	if (signalType == SIGUSR2)
+// 		num = (num * 10) + 1;
+// 	counter++;
+// 	if (counter >= 8)
+// 	{
+// 		c = ft_convert_demical(num);
+// 		write(1, &c, 1);
+// 		num = 0;
+// 		counter = 0;
+// 	}
+// }
+
+// int	main(void)
+// {
+// 	struct sigaction	handler;
+
+// 	handler.sa_handler = ft_handler;
+// 	ft_printf("PID = %d\n", getpid());
+// 	sigemptyset(&handler.sa_mask);
+// 	sigaddset(&handler.sa_mask, SIGUSR1);
+// 	sigaddset(&handler.sa_mask, SIGUSR2);
+// 	sigaction(SIGUSR1, &handler, 0);
+// 	sigaction(SIGUSR2, &handler, 0);
+// 	while (1)
+// 		pause();
+// 	exit(0);
+// }
+
+char	ft_convert_demical(int num, int client_pid)
 {
 	int	demical;
 	int	base;
@@ -29,15 +81,20 @@ char	ft_convert_demical(int num)
 		num = num / 10;
 		base = base * 2;
 	}
+	kill(client_pid, SIGUSR1);
 	return ((char)demical);
 }
 
-void	ft_handler(int signalType)
+void	ft_handler(int signalType, siginfo_t *info, void *context)
 {
-	static int	num;
-	static int	counter;
-	char		c;
+	static int		num;
+	static pid_t	client_pid;
+	static int		counter;
+	char			c;
 
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
 	if (signalType == SIGUSR1)
 		num = (num * 10) + 0;
 	if (signalType == SIGUSR2)
@@ -45,7 +102,7 @@ void	ft_handler(int signalType)
 	counter++;
 	if (counter >= 8)
 	{
-		c = ft_convert_demical(num);
+		c = ft_convert_demical(num, client_pid);
 		write(1, &c, 1);
 		num = 0;
 		counter = 0;
@@ -56,7 +113,7 @@ int	main(void)
 {
 	struct sigaction	handler;
 
-	handler.sa_handler = ft_handler;
+	handler.sa_sigaction = ft_handler;
 	ft_printf("PID = %d\n", getpid());
 	sigemptyset(&handler.sa_mask);
 	sigaddset(&handler.sa_mask, SIGUSR1);
